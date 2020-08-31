5becee8d21878be5e29e7c8599e0246d24d624b5
Ajay <ajayd2k@gmail.com>
2020-08-31 10:57:11 +0530

    31082020

--- Roughwork.py
+++ Roughwork.py
@@ -7,3 +7,15 @@ except AssertionError:
 
 
 
+
+# Code for finding highest Even number in a given list
+
+def highest_even(li):
+    Highest = 0
+    for item in li:
+        if item % 2 == 0:
+            if item > Highest:
+                Highest = item
+    return Highest
+
+print (highest_even([21,22,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]))
