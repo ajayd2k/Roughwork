33ce239092e66fc28908090247660eab2b00b313
Ajay <ajayd2k@gmail.com>
2020-08-31 10:48:28 +0530

    100720201

--- Roughwork.py
+++ Roughwork.py
@@ -1,4 +1,9 @@
 num = int(input('Enter a number greater than 5 : '))
-assert num>5, 'The number entered by you is not greater than 5'
-print('The number greater than 5 entered by you is :', num)
+try:
+    assert num>=5, 'The number entered by you is not greater than 5'
+    print('The number greater than 5 entered by you is :', num)
+except AssertionError:
+    print('The Entered number by you is not greater than 5. Please re-enter the number.')
+
+
 
